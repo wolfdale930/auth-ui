8cb0e9129231b30af940897fa2d52a1fd89ce03e
wolfdale930 <wolfdale930@gmail.com>
2023-09-23 16:08:57 +0530

    improved login page and added register page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,7 @@ const routes: Routes = [
     component: LoginComponent
   },
   {
-    path: 'regsiter',
+    path: 'register',
     component: RegisterComponent
   }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ButtonModule, InputModule } from 'ui';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,6 +15,8 @@ import { RegisterComponent } from './components/register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ButtonModule,
+    InputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
